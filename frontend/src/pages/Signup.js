db75195ecc433416ad0884d08079fb9cd502c38c
arunacharya1603 <arunacharya1603@gmail.com>
2024-08-03 21:20:34 +0530

    updated

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `https://mern-authentication-backend-pi.vercel.app/signup`;
+            const url = `https://mern-authentication-backend-pi.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
