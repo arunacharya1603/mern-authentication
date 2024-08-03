e487e12d06b983fdb256a4de6fcc69f1c000690e
arunacharya1603 <arunacharya1603@gmail.com>
2024-08-03 20:22:18 +0530

    backend connected

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `http://localhost:8080/auth/login`;
+            const url = `https://mern-authentication-backend-pi.vercel.app/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
